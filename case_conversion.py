23cfad09453be62b92ef140dd942d4019ff7a953
Kieran <hiddentiger@gmail.com>
2014-10-05 22:34:31 +1030

    Update case_conversion.py

--- case_conversion.py
+++ case_conversion.py
@@ -5,7 +5,7 @@ import re
 
 def to_ruby_case(text):
     callback = lambda pat: pat.group(1).upper()
-    text = re.sub("::(\w)", callback, text)
+    text = re.sub("(\w)", callback, text)
     if text[0].islower():
         text = text[0].upper() + text[1:]
     return text
